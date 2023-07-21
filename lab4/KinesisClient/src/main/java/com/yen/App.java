81bf64ace1ef4ac111d5f3b280584fe9f6d7bb57
Yen Liu <yenanliu@amazon.com>
2023-07-21 15:17:55 +0100

    update

--- lab4/KinesisClient/src/main/java/com/yen/App.java
+++ lab4/KinesisClient/src/main/java/com/yen/App.java
@@ -1,13 +1,27 @@
 package com.yen;
 
+import com.amazonaws.services.kinesis.AmazonKinesis;
+import com.amazonaws.services.kinesis.model.PutRecordsRequest;
+import com.amazonaws.services.kinesis.model.PutRecordsRequestEntry;
+
+import com.amazonaws.services.kinesis.model.PutRecordsResult;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.yen.consumer.KinesisClient;
 import com.yen.model.Order;
 
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.UUID;
 
 public class App {
     public static void main(String[] args) {
+
+        String KINESIS_STEAM_NAME = "my_kinesis_stream";
+
+        System.out.println(">>> App start");
 
         List<String> productList = new ArrayList<String>();
         populateProductList(productList);
@@ -18,11 +32,19 @@ public class App {
         // step : send event to kinesis
 
         // 1. get client
+        AmazonKinesis kinesisClient = KinesisClient.getKinesisClient();
 
         // 2. PutRecordRequest
+        List<PutRecordsRequestEntry> requestEntryList = getRecordRequestList(orders);
+        PutRecordsRequest recordRequest = new PutRecordsRequest();
+        recordRequest.setStreamName(KINESIS_STEAM_NAME);
+        recordRequest.setRecords(requestEntryList);
 
         // 3. putRecord or putRecords ( 1 record or 500 records in single API call (batch) )
+        PutRecordsResult results = kinesisClient.putRecords(recordRequest);
+        System.out.println(">>> PutRecordsResult = " + results);
 
+        System.out.println(">>> App end");
     }
 
     private static void populateProductList(List<String> productList){
@@ -42,6 +64,18 @@ public class App {
             orders.add(order);
         }
         return orders;
+    }
+
+    private static List<PutRecordsRequestEntry> getRecordRequestList(List<Order> orders){
+        List<PutRecordsRequestEntry> records = new ArrayList<>();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        for (Order order: orders){
+            PutRecordsRequestEntry responseEntry = new PutRecordsRequestEntry();
+            responseEntry.setData(ByteBuffer.wrap(gson.toJson(order).getBytes()));
+            responseEntry.setPartitionKey(UUID.randomUUID().toString());
+            records.add(responseEntry);
+        }
+        return records;
     }
 
 }
