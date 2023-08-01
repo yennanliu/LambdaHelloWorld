86a63f68ecd15c6dbe8f32a1b266a369071982e0
Yen Liu <yenanliu@amazon.com>
2023-08-01 15:29:11 +0100

    fix to batch write s3

--- lab4/KinesisClient/src/main/java/com/yen/kinesis/consumer/ConsumerLambda3.java
+++ lab4/KinesisClient/src/main/java/com/yen/kinesis/consumer/ConsumerLambda3.java
@@ -24,6 +24,8 @@ public class ConsumerLambda3 {
     String BUCKET_NAME = "kinesis-lambda-bucket-test-1";
     String S3_OBJECT_KEY = "my_kinesis_stream_3";
 
+    List<String> data_array = new ArrayList<>();
+
     public String handleRequest(KinesisEvent event, Context context){
 
         System.out.println(">>> context = " + context.toString());
@@ -32,40 +34,36 @@ public class ConsumerLambda3 {
         AmazonS3 s3Client = AmazonS3Client.builder().build();
 
         int cnt = 0;
+        int size = 0;
+        data_array = new ArrayList<>();
 
         for (KinesisEvent.KinesisEventRecord record: event.getRecords()){
             // decode data in kinesis stream
             String data = StandardCharsets.UTF_8.decode(record.getKinesis().getData()).toString();
             System.out.println("--> data = " + data);
             cnt += 1;
-            //Boolean writeS3Failure = false;
-
-            List<String> data_array = new ArrayList<>();
-
-            try{
-                // write to S3
-                // https://medium.com/@olayiwolafunsho/serverless-with-aws-lambda-and-java-generate-an-excel-file-and-upload-to-s3-a21005a015a2
-                // https://docs.aws.amazon.com/AmazonS3/latest/userguide/olap-writing-lambda.html
-                
-                long unixtime = System.currentTimeMillis() / 1000L;
-                String filename = Long.toString(unixtime) + ".txt";
-
-                System.out.println(">>> write to s3 start");
-                ObjectMetadata metadata = new ObjectMetadata();
-                metadata.setContentLength(data.toString().length());
-                InputStream inputStream = new ByteArrayInputStream(data.toString().getBytes());
-                PutObjectRequest putRequest = new PutObjectRequest(BUCKET_NAME, S3_OBJECT_KEY + "/" + filename, inputStream, metadata);
-                s3Client.putObject(putRequest);
-            }catch (Exception e){
-                //writeS3Failure = true;
-                System.out.println(">>> write to S3 failed : " + e.toString());
-            }
-
-
+            size += data.length();
+            data_array.add(data.toString());
         }
 
         System.out.println("--> process " + cnt + " kinesis records");
 
+        try{
+            // write to S3
+            // https://medium.com/@olayiwolafunsho/serverless-with-aws-lambda-and-java-generate-an-excel-file-and-upload-to-s3-a21005a015a2
+            // https://docs.aws.amazon.com/AmazonS3/latest/userguide/olap-writing-lambda.html
+
+            long unixtime = System.currentTimeMillis() / 1000L;
+            String filename = Long.toString(unixtime) + ".txt";
+            System.out.println(">>> write to s3 start");
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentLength(size);
+            InputStream inputStream = new ByteArrayInputStream(data_array.toString().getBytes());
+            PutObjectRequest putRequest = new PutObjectRequest(BUCKET_NAME, S3_OBJECT_KEY + "/" + filename, inputStream, metadata);
+            s3Client.putObject(putRequest);
+        }catch (Exception e){
+            System.out.println("write to s3 failed " + e);
+        }
         return "OK";
     }
 
