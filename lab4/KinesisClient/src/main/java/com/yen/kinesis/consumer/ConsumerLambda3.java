8acd49e1847da5e7e3846cd40ef3e24360f1f868
Yen Liu <yenanliu@amazon.com>
2023-08-01 14:52:31 +0100

    update

--- lab4/KinesisClient/src/main/java/com/yen/kinesis/consumer/ConsumerLambda3.java
+++ lab4/KinesisClient/src/main/java/com/yen/kinesis/consumer/ConsumerLambda3.java
@@ -2,16 +2,34 @@ package com.yen.kinesis.consumer;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.KinesisEvent;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
-/** consume Kinesis : my_kinesis_stream_3 */
+/** consume
+ *
+ * Lambda func : ConsumerLambda3
+ * Kinesis name : my_kinesis_stream_3
+ *
+ */
 public class ConsumerLambda3 {
+
+    String BUCKET_NAME = "kinesis-lambda-bucket-test-1";
+    String S3_OBJECT_KEY = "my_kinesis_stream_3";
 
     public String handleRequest(KinesisEvent event, Context context){
 
         System.out.println(">>> context = " + context.toString());
         System.out.println(">>> event = " + event.toString());
+
+        AmazonS3 s3Client = AmazonS3Client.builder().build();
 
         int cnt = 0;
 
@@ -20,11 +38,39 @@ public class ConsumerLambda3 {
             String data = StandardCharsets.UTF_8.decode(record.getKinesis().getData()).toString();
             System.out.println("--> data = " + data);
             cnt += 1;
+            //Boolean writeS3Failure = false;
+
+            List<String> data_array = new ArrayList<>();
+
+            try{
+                // write to S3
+                // https://medium.com/@olayiwolafunsho/serverless-with-aws-lambda-and-java-generate-an-excel-file-and-upload-to-s3-a21005a015a2
+                // https://docs.aws.amazon.com/AmazonS3/latest/userguide/olap-writing-lambda.html
+                
+                long unixtime = System.currentTimeMillis() / 1000L;
+                String filename = Long.toString(unixtime) + ".txt";
+
+                System.out.println(">>> write to s3 start");
+                ObjectMetadata metadata = new ObjectMetadata();
+                metadata.setContentLength(data.toString().length());
+                InputStream inputStream = new ByteArrayInputStream(data.toString().getBytes());
+                PutObjectRequest putRequest = new PutObjectRequest(BUCKET_NAME, S3_OBJECT_KEY + "/" + filename, inputStream, metadata);
+                s3Client.putObject(putRequest);
+            }catch (Exception e){
+                //writeS3Failure = true;
+                System.out.println(">>> write to S3 failed : " + e.toString());
+            }
+
+
         }
 
         System.out.println("--> process " + cnt + " kinesis records");
 
         return "OK";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(System.currentTimeMillis() / 1000L);
     }
 
 }
